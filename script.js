1f089044401f0901b5a4b37ae75f6291a87324cf
Grady Ku <gradku@pdx.edu>
2020-10-25 14:02:32 -0700

    Updated script.js
    Added placeholder functionality to all buttons.
    
    Implemented the 'add' button.
    
    Added new <li> and <ol> code in script.js

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
+  // 'Add' button - adds a new item to list
 var addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addToDoItem);
+  // 'Clear' button - clears all completed items from list
+var clearButton = document.getElementById("clear-completed-button");
+clearButton.addEventListener("click", clearCompleted);
+  // 'Delete' button - delete all items in list
+var deleteButton = document.getElementById("delete-button");
+deleteButton.addEventListener("click", deleteList);
+  // 'Save' button - save current items in list
+var saveButton = document.getElementById("save-button");
+saveButton.addEventListener("click", saveList);
+  // Text box
+var textBox = document.getElementById("todo-entry-box");
+  // Todo list
+var toDoList = document.getElementById("todo-list");
 
+// 'Add'
 function addToDoItem() {
-  alert("Add button was clicked!");
+  var itemDesc = textBox.value; //store the text box value into itemDesc
+  newToDoItem(itemDesc, false); //call our function & add to list
+}
+
+// 'Clear'
+function clearCompleted() {
+  alert("Cleared all completed items from list!");
+}
+
+// 'Delete'
+function deleteList() {
+  alert("All items in list have been deleted.");
+}
+
+// 'Save'
+function saveList() {
+  alert("List saved!");
+}
+
+// Adds a new to-do item to list
+function newToDoItem(itemDesc, completed) {
+  var toDoItem = document.createElement("li"); //create a new "li" element to use in place of <li>
+  var toDoText = document.createTextNode(itemDesc); //create a new Text Node container & store the contents of itemDesc in it
+  toDoItem.appendChild(toDoText); //add the toDoText Text Node as a list item <li>
+
+  if (completed) { // if completed is TRUE, adds a class called "completed" to this list item which changes its appearance
+    toDoItem.classList.add("completed");
+  }
+
+  toDolist.appendChild(toDoItem); //add the toDoItem list to the ordered list <ol>
+  toDoItem.addEventListener("dblclick", toggleToDoItemState); //upon double-click, the <li> calls the function
 }
