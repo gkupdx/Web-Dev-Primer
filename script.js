98882c0383fcfbbbe45fb1c74cc492fc9c62f61a
Grady Ku <gradku@pdx.edu>
2020-10-25 17:01:04 -0700

    Updated script.js
    Moved newToDoItem and toggleItemState function implementations closer to the add item implementation.
    
    Implemented loadList function.

--- script.js
+++ script.js
@@ -21,6 +21,29 @@ function addToDoItem() {
   newToDoItem(itemDesc, false); //call our function & add to list
 }
 
+// Adds a new to-do item to list
+function newToDoItem(itemDesc, completed) {
+  var toDoItem = document.createElement("li"); //create a new "li" element to use in place of <li>
+  var toDoText = document.createTextNode(itemDesc); //create a new TextNode container & store the contents of itemDesc in it
+  toDoItem.appendChild(toDoText); //add the toDoText Text Node as a list item <li>
+
+  if (completed) { // if completed is TRUE, adds a class called "completed" to this list item which changes its appearance
+    toDoItem.classList.add("completed");
+  }
+
+  toDoList.appendChild(toDoItem); //add the toDoItem list to the ordered list <ol>
+  toDoItem.addEventListener("dblclick", toggleItemState); //upon double-click, the <li> calls the function
+}
+
+// Toggles current item between 'completed' and 'not completed'
+function toggleItemState() {
+  if (this.classList.contains("completed")) { //if current item is 'completed', toggle to other state
+    this.classList.remove("completed");
+  } else { //current item is not completed, toggle to 'completed'
+    this.classList.add("completed");
+  }
+}
+
 // 'Clear'
 function clearCompleted() {
   var completedItems = toDoList.getElementsByClassName("completed"); //set 'completedItem' to all IDs labeled with class name "completed"
@@ -58,25 +81,16 @@ function saveList() {
   localStorage.setItem("todos", JSON.stringify(tasks)); //saves list to local storage via JSON stringify
 }
 
-// Adds a new to-do item to list
-function newToDoItem(itemDesc, completed) {
-  var toDoItem = document.createElement("li"); //create a new "li" element to use in place of <li>
-  var toDoText = document.createTextNode(itemDesc); //create a new TextNode container & store the contents of itemDesc in it
-  toDoItem.appendChild(toDoText); //add the toDoText Text Node as a list item <li>
+// 'Load'
+function loadList() {
+  if (localStorage.getItem("todos") != null) { //"todos" key does indeed exist
+    var todos = JSON.parse(localStorage.getItem("todos"));
 
-  if (completed) { // if completed is TRUE, adds a class called "completed" to this list item which changes its appearance
-    toDoItem.classList.add("completed");
-  }
-
-  toDoList.appendChild(toDoItem); //add the toDoItem list to the ordered list <ol>
-  toDoItem.addEventListener("dblclick", toggleItemState); //upon double-click, the <li> calls the function
-}
-
-// Toggles current item between 'completed' and 'not completed'
-function toggleItemState() {
-  if (this.classList.contains("completed")) { //if current item is 'completed', toggle to other state
-    this.classList.remove("completed");
-  } else { //current item is not completed, toggle to 'completed'
-    this.classList.add("completed");
+    for (var i = 0; i < todos.length; ++i) { //loop through and load each saved item
+      var tasks = todos[i];
+      newToDoItem(tasks.task, tasks.completed);
+    }
   }
 }
+
+loadList();
