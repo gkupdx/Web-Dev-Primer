92672406d072e76dd188aaee354ec74e9993d508
Grady Ku <gradku@pdx.edu>
2020-10-27 20:47:13 -0700

    Updated files
    Deleted sweetscroll.html file and reverted back to original to-do list.

--- script.js
+++ script.js
@@ -15,11 +15,6 @@ var textBox = document.getElementById("todo-entry-box");
   // Todo list
 var toDoList = document.getElementById("todo-list");
 
-/* Click on 'Cake Recipe' button to go to cake recipe page
-document.getElementById('recipe-button').onclick = function() {
-  location.href = "sweetscroll.html"
-}*/
-
 // 'Add'
 function addToDoItem() {
   var itemDesc = textBox.value; //store the text box value into itemDesc
